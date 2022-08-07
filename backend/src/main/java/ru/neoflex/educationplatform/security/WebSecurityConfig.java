fec05ccb120193d747cc19610b0b97c255dcec73
Ksusha <ksushabass@gmail.com>
2022-08-07 13:23:12 +0300

    попытка победить корсы

--- backend/src/main/java/ru/neoflex/educationplatform/security/WebSecurityConfig.java
+++ backend/src/main/java/ru/neoflex/educationplatform/security/WebSecurityConfig.java
@@ -12,11 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -30,7 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
   protected void configure(HttpSecurity http) throws Exception {
 
     http
-            .cors().configurationSource(corsConfigurationSource()).and()
+            .cors().and()
+            .csrf().disable()
             .authorizeRequests()
             .antMatchers("/users/login").permitAll()
             .antMatchers("/users/registration").permitAll()
@@ -38,16 +34,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
             .anyRequest().authenticated()
             .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
   }
 
   @Override
